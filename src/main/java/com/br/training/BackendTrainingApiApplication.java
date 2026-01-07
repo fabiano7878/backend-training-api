b06147c5f8628cb5bafcd3a9e214b359ff7ebffc
Fabiano <fabiano.asistemas@gmail.com>
2026-01-07 18:30:13 -0300

    primeiro passo

---
+++ src/main/java/com/br/training/BackendTrainingApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTrainingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTrainingApiApplication.class, args);
+	}
+
+}
